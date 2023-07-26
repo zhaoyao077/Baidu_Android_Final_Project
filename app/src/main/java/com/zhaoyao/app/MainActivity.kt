496b03a1fdd1b3d0a36f959e771ea2091461d78c
zhaoyao <zhaoyao@smail.nju.edu.cn>
2023-07-26 16:29:38 +0800

    调整新闻RecycleView的布局

--- app/src/main/java/com/zhaoyao/app/MainActivity.kt
+++ app/src/main/java/com/zhaoyao/app/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initText1() {
-        repeat(2) {
+        repeat(1) {
             textList1.add(NewsText1("这些重大问题，习近平的回答掷地有声", "置顶", "新华社新媒体"))
             textList1.add(NewsText1("习近平会见”元老会“代表团", "置顶", "央视网新闻"))
         }
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initText2() {
-        repeat(3) {
+        repeat(4) {
             textList2.add(
                 NewsText2(
                     "美媒：五角大楼盯上谷歌在华AI中心 谷歌忙安抚",
